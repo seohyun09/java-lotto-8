8f1b364d5b0851ad063ece7447e06f988aa79e0d
이서현 <mellifluous3380@gmail.com>
2025-11-02 16:46:40 +0900

    feat: ã구입 금액 입력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputPrice = Console.readLine();
+        PriceValidator.validatePlusPrice(inputPrice);
+
+        int price = Integer.parseInt(inputPrice);
+        PriceValidator.validatePriceUnit(price);
+
+        int buyCount = price / 1000;
+        System.out.println(buyCount + "개를 구매했습니다.");
+
+
     }
 }
