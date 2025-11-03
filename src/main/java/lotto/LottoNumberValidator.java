9853fb9401be9e55feebc667bfd48d79e6f33887
이서현 <mellifluous3380@gmail.com>
2025-11-03 17:18:50 +0900

    feat: 당첨 번호 검증 로직 구현 및 클래스 분리

--- src/main/java/lotto/LottoNumberValidator.java
+++ src/main/java/lotto/LottoNumberValidator.java
@@ -1,12 +1,41 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class LottoNumberValidator {
 
-    public static void validateNumOfWinningNumber(String inputWinningNumbers) {
+    public static String[] validateNumOfWinningNumber(String inputWinningNumbers) {
         String[] winningNumbers = inputWinningNumbers.split(",");
         if (winningNumbers.length != 6) {
             throw new IllegalArgumentException("당첨 번호 개수가 6개가 아닙니다.");
         }
+        return winningNumbers;
     }
 
+    public static void validateUniqueWinningNumber(String[] winningNumbers) {
+        int[] numbers = new int[winningNumbers.length];
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (set.contains(winningNumbers[i])) {
+                throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
+            }
+            set.add(winningNumbers[i]);
+
+            numbers[i] = validateWinningNumberRange(winningNumbers[i]);
+        }
+    }
+
+    public static int validateWinningNumberRange(String winningNumber) {
+        if (!winningNumber.matches("^\\d+")) {
+            throw new IllegalArgumentException("입력한 당첨 번호는 양의 정수이어야 합니다.");
+        }
+
+        int number = Integer.parseInt(winningNumber);
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("당첨 번호는 1과 45 사이의 숫자이어야 합니다.");
+        }
+        return number;
+    }
 }
