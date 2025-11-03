dd8ed60888d2769828072dba0d63d61cdcba2d23
이서현 <mellifluous3380@gmail.com>
2025-11-03 19:14:46 +0900

    feat: 당첨 통계 출력 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
@@ -48,6 +47,8 @@ public class Application {
             int rank = LottoResultChecker.LottoMatch(lottos.get(i), numbers, bonusNumber);
             LottoRank[rank]++;
         }
-        System.out.println(Arrays.toString(LottoRank));
+
+        LottoResultChecker.printLottoResult(LottoRank);
+
     }
 }
--- src/main/java/lotto/LottoResultChecker.java
+++ src/main/java/lotto/LottoResultChecker.java
@@ -46,5 +46,11 @@ public class LottoResultChecker {
         return 0;
     }
 
-
+    public static void printLottoResult(int[] LottoRank) {
+        System.out.println("3개 일치 (5,000원) - " + LottoRank[5] + "개");
+        System.out.println("4개 일치 (50,000원) - " + LottoRank[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + LottoRank[3] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + LottoRank[2] + "개");
+        System.out.println("6개 일치 (2,000,000원) - " + LottoRank[1] + "개");
+    }
 }
